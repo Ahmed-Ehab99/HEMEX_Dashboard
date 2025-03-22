a642276820994c99174fb096030481bda042fa9e
Ahmed-Ehab99 <ahmedehab1677@gmail.com>
2025-03-22 15:15:04 +0200

    update images config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["backapi.hemmx.org"],
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "backapi.hemmx.org",
+      },
+    ],
   },
 };
 
